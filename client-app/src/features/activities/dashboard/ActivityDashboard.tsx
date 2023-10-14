75ffdcbb091609eeebac4e2352d8ed049711faf1
Julio Hahn <julio.hahn@gmail.com>
2023-10-14 19:58:35 -0400

    AB#1837: Create the Activity Dashboard

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,23 @@
+import {} from 'react';
+import {Grid, List} from "semantic-ui-react";
+import { Activity } from '../../../app/models/activity';
+
+interface Props {
+    activities: Activity[];
+}
+
+export default function ActivityDashboard({activities}: Props) {
+    return(
+        <Grid>
+            <Grid.Column width='10'>
+                <List>
+                    {activities.map(activity => (
+                        <List.Item key={activity.id}>
+                            {activity.title}
+                        </List.Item>
+                    ))}
+                </List>
+            </Grid.Column>
+        </Grid>
+    )
+}
