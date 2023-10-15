ab268196422353a8e3edaff1bb04d8ad2a132e2d
Julio Hahn <julio.hahn@gmail.com>
2023-10-14 21:21:54 -0400

    AB#1839: Created the Activity Details Component

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,29 @@
+import {} from 'react';
+import { Activity } from '../../../app/models/activity';
+import {Button, Card, Image} from 'semantic-ui-react';
+
+interface Props{
+    activity: Activity
+}
+export default function ActivityDetails({activity}: Props) {
+    return (
+        <Card fluid>
+            <Image src={`/assets/Images/categoryImages/${activity.category}.jpg`} />
+            <Card.Content>
+                <Card.Header>{activity.title}</Card.Header>
+                <Card.Meta>
+                    <span className='date'>{activity.date}</span>
+                </Card.Meta>
+                <Card.Description>
+                    {activity.description}
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button.Group widths='2'>
+                    <Button basic color='blue' content='Edit' />
+                    <Button basic color='grey' content='Cancel' />
+                </Button.Group>
+            </Card.Content>
+        </Card>
+    )
+}
