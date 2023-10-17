73eda78308c217e90d63a890da6a22929ef83c5b
Julio Hahn <julio.hahn@gmail.com>
2023-10-17 18:25:06 -0400

    AB#1865: Styling the List Items

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,19 +1,25 @@
-import {Item, Segment} from "semantic-ui-react";
+import {Header, Item, Segment} from "semantic-ui-react";
 import { useStore } from '../../../app/stores/store';
 import {observer} from "mobx-react-lite";
 import ActivityListItem from "./ActivityListItem";
+import {Fragment} from "react";
 
 export default observer(function ActivityList(){
     const {activityStore} = useStore();
-    const {activitiesByDate: activities} = activityStore;
+    const {groupedActivities} = activityStore;
     
     return(
-        <Segment>
-            <Item.Group divided>
-                {activities.map(activity => (
-                    <ActivityListItem key={activity.id} activity={activity} />
-                ))}
-            </Item.Group>
-        </Segment>
+        <Fragment>
+            {groupedActivities.map(([group, activities]) => (
+                <Fragment key={group}>
+                    <Header sub color='teal'>
+                        {group}
+                    </Header>
+                    {activities.map(activity => (
+                        <ActivityListItem key={activity.id} activity={activity} />
+                    ))}
+                </Fragment>
+            ))}
+        </Fragment>
     )
 });
