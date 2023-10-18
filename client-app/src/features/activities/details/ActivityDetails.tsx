ea11ce90d110e3dbf0f28e3b32c8bb229d890e38
Julio Hahn <julio.hahn@gmail.com>
2023-10-17 21:17:24 -0400

    AB#1866: Activity Details component definitions

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,9 +1,13 @@
 import {useEffect} from 'react';
-import {Button, Card, Image} from 'semantic-ui-react';
+import {Button, Card, Grid, GridColumn, Image} from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import {observer} from "mobx-react-lite";
 import {Link, useParams} from "react-router-dom";
 import LoadingComponent from '../../../app/layout/LoadingComponent';
+import ActivityDetailedHeader from './ActivityDetailedHeader';
+import ActivityDetailedChat from './ActivityDetailedChat';
+import ActivityDetailedInfo from './ActivtyDetailedInfo';
+import ActivityDetailedSidebar from './ActivityDetailedSidebar';
 
 export default observer(function ActivityDetails() {
     const {activityStore} = useStore();
@@ -19,23 +23,15 @@ export default observer(function ActivityDetails() {
     if(loadingInitial || !activity) return <LoadingComponent />;
     
     return (
-        <Card fluid>
-            <Image src={`/assets/Images/categoryImages/${activity.category}.jpg`} />
-            <Card.Content>
-                <Card.Header>{activity.title}</Card.Header>
-                <Card.Meta>
-                    <span className='date'>{activity.date}</span>
-                </Card.Meta>
-                <Card.Description>
-                    {activity.description}
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <Button.Group widths='2'>
-                    <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit' />
-                    <Button as={Link} to='/activities' basic color='grey' content='Cancel' />
-                </Button.Group>
-            </Card.Content>
-        </Card>
+        <Grid>
+            <GridColumn width={10}>
+                <ActivityDetailedHeader />
+                <ActivityDetailedInfo />
+                <ActivityDetailedChat />
+            </GridColumn>
+            <Grid.Column width={6}>
+                <ActivityDetailedSidebar />
+            </Grid.Column>
+        </Grid>
     )
 });
---
+++ client-app/src/features/activities/details/ActivtyDetailedInfo.tsx
@@ -0,0 +1,5 @@
+export default function ActivtyDetailedInfo() {
+    return(
+        <h1>Info</h1>
+    )
+}
