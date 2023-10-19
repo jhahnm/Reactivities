5f2c30b794093debec33ae9884c627f3b403b102
Julio Hahn <julio.hahn@gmail.com>
2023-10-19 19:27:19 -0400

    AB#1873: Handle empty responses in a specific Base method in the BaseController for empty responses.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -16,15 +16,9 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        public async Task<IActionResult> GetActivity(Guid id)
         {
-            var activity = await Mediator.Send(new Details.Query { Id = id });
-            if (activity == null)
-            {
-                return NotFound();
-            }
-
-            return activity;
+            return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
         }
 
         [HttpPost]
