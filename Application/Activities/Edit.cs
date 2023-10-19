f1beb98378de63d6f1fbc10d672b705809a03990
Julio Hahn <julio.hahn@gmail.com>
2023-10-18 23:03:38 -0400

    AB#1872: Configure Fluent Validation

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -10,6 +11,13 @@ public class Edit
     public class Command: IRequest
     {
         public Activity Activity { get; set; }
+    }
+    public class CommandValidator : AbstractValidator<Command>
+    {
+        public CommandValidator()
+        {
+            RuleFor(c => c.Activity).SetValidator(new ActivityValidator());
+        }
     }
     
     public class Handler: IRequestHandler<Command>
