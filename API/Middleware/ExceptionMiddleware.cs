f1fd7dcb50128fba42e6091407c492006c9fd312
Julio Hahn <julio.hahn@gmail.com>
2023-10-19 21:31:19 -0400

    AB#1874: Handling Exceptions with a middleware

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+using System.Net;
+using System.Text.Json;
+using Application.Core;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IHostEnvironment _env;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+    {
+        _env = env;
+        _logger = logger;
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new AppException(context.Response.StatusCode, e.Message,
+                _env.IsDevelopment() ? e.StackTrace?.ToString() : "Internal Server Error");
+
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            
+            var json = JsonSerializer.Serialize(response, options);
+
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
