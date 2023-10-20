aca58e0b92743cba44c021686bc8625f93d132e7
Julio Hahn <julio.hahn@gmail.com>
2023-10-20 13:07:38 -0400

    AB#1875: Preparing the UI to handle and show toaster for failures

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -23,7 +23,7 @@ public class Delete
         public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
         {
             var activity = await _context.Activities.FindAsync(request.Id);
-            
+            if (activity == null) return null;
             _context.Remove(activity);
             if (!(await _context.SaveChangesAsync(cancellationToken) > 0))
             {
